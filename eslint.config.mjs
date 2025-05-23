faba1670e6353ad466f66569db4db794910abdca
ezio.santori <ezio.santori@gmail.com>
2025-05-23 20:07:57 +0200

    chore(eslint)t: enhance ESLint configuration with import and unused-imports plugins

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,8 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import importPlugin from "eslint-plugin-import";
+import unusedImports from "eslint-plugin-unused-imports";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -11,6 +13,22 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    plugins: {
+      import: importPlugin,
+      "unused-imports": unusedImports
+    },
+    rules: {
+      "indent": ["warn", 2], // Indentazione di 2 spazi
+      "import/order": ["warn", {
+        "groups": [["builtin", "external", "internal"]],
+        "newlines-between": "always"
+      }],
+      "no-console": ["warn", { allow: ["warn", "error"] }],
+      "@typescript-eslint/no-unused-vars": "error",
+      "unused-imports/no-unused-imports": "warn"
+    }
+  },
 ];
 
 export default eslintConfig;
